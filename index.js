41239d393d67b8bfb2b7e9f6b0d221685cb9dabb
Ben Nesbit <benesbit@yahoo.com>
2020-03-08 13:38:17 -0700

    Add template code for express app with AWS

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AWS = require('aws-sdk');
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Music app listening on port ${PORT}!`));
